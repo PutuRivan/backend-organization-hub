3f2bfa5da52834f36254cc059e143b26a9e31706
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2025-11-11 21:58:38 +0700

    set up handler and prisma

---
+++ middlewares/authorization.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
+  }
+}
+
+function authorization(roles: string[]) {
+  return function (req: Request, res: Response, next: NextFunction) {
+    try {
+      let token: string | undefined = '';
+      let user: any = { role: 'guest' };
+
+      if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        token = req.headers.authorization.split(' ')[1];
+
+        if (token !== null && token !== undefined && token !== '') {
+          user = jwt.verify(token, process.env.JWT_SECRET!);
+          console.log({ user })
+          req.user = user;
+        } else {
+          user = { role: 'guest' };
+        }
+
+      }
+
+      if (!roles.includes(user.role)) {
+        throw new Error('You are not authorized with this token');
+      }
+      console.log(user.role)
+      next();
+    } catch (error: any) {
+      res.status(401).json({ message: 'Authorization failed', error: error.message });
+    }
+  };
+}
+
+export default authorization;
